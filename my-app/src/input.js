9ccc0a9a789c3bbec71b5f2bb8fe8aebf6ef9d96
David Waldherr <david.a.waldherr@vanderbilt.edu>
2022-10-20 21:55:11 -0500

    currently can add a period to the end of sentences

--- my-app/src/input.js
+++ my-app/src/input.js
@@ -3,15 +3,32 @@ import React from 'react';
 function Input({onTextChange}) {
     const [text, setText] = React.useState("");
     
+    // Write a Javascript function that accepts a var text input and splits it into a list containing the
+    // lines in the input. The function should return the list.
+    function splitText(text) {
+        return text.split("\n");
+    }
+
     function appendText(e) {
         const file = e.target.files[0];
         const reader = new FileReader();
         
         reader.onload = (e) => {
-        var text = (e.target.result); // This is the text file
-        text = "Hello there.\n" + text;
-        setText(text); // This is the new text file
-        onTextChange(text);
+        var text = (e.target.result); // This is the text file  
+        // Define content
+        var content = splitText(text);
+
+        // Iterate through the list. If a line does not end with a ".", "!", or "."", or "?", append a period to the end of the line.
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].endsWith(".") || content[i].endsWith("!") || content[i].endsWith("?")) {
+                continue;
+            } else {
+                content[i] = content[i] + ".";
+            }
+        }
+        // Join the list into a string and set the state of the text variable to the string.
+        setText(content.join("\n"));
+        onTextChange(content.join("\n"));
         };
 
         reader.readAsText(file); // read the file
